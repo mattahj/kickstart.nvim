3d97b83186e2feb68a09f2dfd16ed6903480b3bf
Matthew Jennings <matt@mattahj.co.uk>
2024-09-09 11:25:48 +0300

    added shortcut to show lsp errors

--- init.lua
+++ init.lua
@@ -187,6 +187,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- show lsp errors in a window
+vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', { desc = 'Show error text' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
